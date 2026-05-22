8f3d502ed2276b690c4454659798f353dfe71841
HuaLiMao-AQ <hualimao_aq@163.com>
2026-05-22 15:56:00 +0800

    feat: 调整样式，将文章调整到外部
    Signed-off-by: HuaLiMao-AQ <hualimao_aq@163.com>

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -14,7 +14,6 @@ export const sharedPageComponents: SharedLayout = {
   }),
 }
 
-// components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
     Component.ConditionalRender({
@@ -38,16 +37,22 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
-    Component.Explorer(),
   ],
   right: [
-    Component.Graph(),
+    Component.ConditionalRender({
+      component: Component.Graph(),
+      condition: (page: any) => {
+        if (page.fileData.slug === "index") return false
+
+        const outgoing = page.fileData.links?.length ?? 0
+        return outgoing > 0
+      },
+    }),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
 }
 
-// components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
   beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
   left: [
@@ -62,7 +67,6 @@ export const defaultListPageLayout: PageLayout = {
         { Component: Component.Darkmode() },
       ],
     }),
-    Component.Explorer(),
   ],
   right: [],
 }
